870deba3c0a594dd49eb70d63d3a68f0a7f24d7f
devonwintz <samuelwintz@gmail.com>
2023-03-28 13:18:15 -0400

    Added basic login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./components/Login/Login"
 
 function App() {
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
